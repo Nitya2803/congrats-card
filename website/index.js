921eadcb3f3b0ee5dda78092cd7682547966c00f
Nitya2803 <141040110+Nitya2803@users.noreply.github.com>
2023-09-05 23:00:08 +0530

    Add files via upload

---
+++ website/index.js
@@ -0,0 +1,19 @@
+const element = (
+    <div className="container">
+      <h1 className="heading">Congratulations</h1>
+      <div className="card">
+        <img
+          src="https://tse1.mm.bing.net/th?id=OIP.7UuF5-ZpyU_IC2yxV_g4GwHaEo&pid=Api&P=0&h=180"
+          alt="person"
+        />
+        <h1 className="heading1">A.J.Nitya shree</h1>
+        <p className="para">I am studying in Vignan's institute of engineering for women's</p>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          alt="watch"
+        />
+      </div>
+    </div>
+  );
+  
+  ReactDOM.render(element, document.getElementById("root"));
